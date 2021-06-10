2e95a3568db532dd8216ad2d965ed42f869d1df3
Raf <rafaelmendoza004@gmail.com>
2021-06-10 12:24:21 +0800

    first commit for alodi arts blog

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>The best wayy to reach me is via github and twittter</p>
+    </div>
+  )
+}
+
+export default Contact
