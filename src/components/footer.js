aafdf1cf015908f016e6cd3ec3b8b106de6ae56d
Raf <rafaelmendoza004@gmail.com>
2021-06-11 23:25:31 +0800

    query on siteMetadata

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,20 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
+
   return (
     <footer>
-      <p>Created By Rafael Mendoza, © 2021</p>
+      <p>Created By {data.site.siteMetadata.author}, © 2021</p>
     </footer>
   )
 }
