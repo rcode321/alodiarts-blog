0bb260a4732f746839942d9784ee0f45ac41282f
Raf <rafaelmendoza004@gmail.com>
2021-06-11 20:07:32 +0800

    change link className to nav-item

--- src/components/header.js
+++ src/components/header.js
@@ -14,18 +14,24 @@ const Header = () => {
       <nav>
         <ul className={headerStyles.navList}>
           <li>
-            <Link className={headerStyles.link} to="/">
+            <Link className={headerStyles.navItem} to="/">
               Home
             </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link className={headerStyles.navItem} to="/about">
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link className={headerStyles.navItem} to="/blog">
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link className={headerStyles.navItem} to="/contact">
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
